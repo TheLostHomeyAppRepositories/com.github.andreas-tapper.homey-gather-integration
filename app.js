f50de02684c2a625dfa9050f799a7c8ee7977d95
Andreas Tapper <andreas.tapper@ctwo.com>
2024-03-08 18:07:55 +0100

    Working cards and presence sensing.

--- app.js
+++ app.js
@@ -6,65 +6,216 @@ const { Game, ServerClientEvent } = require('@gathertown/gather-game-client');
 global.WebSocket = require("isomorphic-ws");
 
 class GatherApp extends Homey.App {
+  // Homey cards
+  #incomingCallCard;
+  #incomingWaveCard;
+  #connectionStatusCard;
+  #presenceStatusCard;
 
-  /**
-   * onInit is called when the app is initialized.
-   */
-  async onInit() {
-    this.log('Starting initialization of the Gather integration.');
+  // Gather integration
+  #game;
+  #avatarName;
+  #player;
+  #playerPrivateSpaceId;
 
+  get me() {
     const self = this;
-    const apiKey = this.homey.settings.get("gatherToken") || Homey.env.GATHER_TOKEN;
-    const spaceId = this.homey.settings.get("spaceId") || Homey.env.SPACE_ID;
-    const avatarName = this.homey.settings.get("avatarName") || Homey.env.AVATAR_NAME;
-    let isAlone = true;
+
+    if (self.#player) {
+      return self.#player;
+    }
+
+    const players = this.#game.filterPlayersInSpace(player => player.name == self.#avatarName);
+    return self.#player = (players.length > 0 ? players[0] : null);
+  }
+
+  get myPrivateSpaceId() {
+    const self = this;
+
+    if (self.#playerPrivateSpaceId) {
+      return self.#playerPrivateSpaceId;
+    }
+
+    return self.#playerPrivateSpaceId = self.#gatherPlayerInPrivateSpace(self.me);
+  }
+
+  async onInit() {
+    const self = this;
+
+    self.log('Starting initialization of the Gather integration.');
 
     // Flow cards
-    const incomingCallCard = this.homey.flow.getTriggerCard("incoming-call");
-    const incomingWaveCard = this.homey.flow.getTriggerCard("incoming-wave");
-    const connectionStatusCard = this.homey.flow.getTriggerCard("connection-status");
-    const notAloneCard = this.homey.flow.getTriggerCard("not-alone");
+    self.#incomingCallCard = self.homey.flow.getTriggerCard("incoming-call");
+    self.#incomingWaveCard = self.homey.flow.getTriggerCard("incoming-wave");
+    self.#connectionStatusCard = self.homey.flow.getTriggerCard("connection-status");
+    self.#presenceStatusCard = self.homey.flow.getTriggerCard("presence-status");
+
+    const aloneConditionCard = self.homey.flow.getConditionCard("youre-alone");
+    aloneConditionCard.registerRunListener(async () => {
+      return self.me?.isAlone == true;
+    });
 
     // Gather integration
-    const game = new Game(spaceId, () => Promise.resolve({ apiKey: apiKey }));
-    game.connect();
-    game.subscribeToConnection((connected) => {
+    self.#avatarName = self.homey.settings.get("avatarName") || Homey.env.AVATAR_NAME;
+    await self.connectToGather();
+
+    self.log('Initialization of the Gather integration completed.');
+  }
+
+  async onUninit() {
+    await this.disconnectFromGather();
+  }
+
+  async connectToGather() {
+    const self = this;
+
+    if (!!self.#game) {
+      self.log("Gather game already initiated.")
+      return;
+    }
+
+    const apiKey = this.homey.settings.get("gatherToken") || Homey.env.GATHER_TOKEN;
+    const spaceId = this.homey.settings.get("spaceId") || Homey.env.SPACE_ID;
+
+    self.#game = new Game(spaceId, () => Promise.resolve({ apiKey: apiKey }));
+    self.#game.subscribeToConnection(self.#gatherConnection());
+    self.#game.subscribeToDisconnection(self.#gatherDisconnection());
+    self.#game.subscribeToEvent("ready", self.#gatherIsReady());
+    self.#game.subscribeToEvent("playerMoves", self.#gatherPlayerMoves());
+    self.#game.subscribeToEvent("playerRings", self.#gatherPlayerRings());
+    self.#game.subscribeToEvent("playerSetsIsAlone", self.#gatherPlayerSetsIsAlone());
+    self.#game.subscribeToEvent("playerWaves", self.#gatherPlayerWaves());
+
+    this.#game.connect();
+  }
+
+  async disconnectFromGather() {
+    if (!game) {
+      return;
+    }
+
+    await this.#game.disconnect();
+    this.#game = null;
+  }
+
+  #gatherConnection() {
+    const self = this;
+    return (connected) => {
       self.log(`Gather connection ${(connected ? 'established' : 'failed')}.`);
-      connectionStatusCard.trigger({ connected: connected });
-    });
+      self.#connectionStatusCard.trigger({ connected: connected });
+    };
+  }
 
-    game.subscribeToEvent("playerRings", (data, context) => {
+  #gatherDisconnection() {
+    const self = this;
+    return (code, reason) => {
+      self.log(`Gather was disconnected '${reason}' (${code}).`);
+    };
+  }
+
+  #gatherIsReady() {
+    const self = this;
+    return (data, context) => {
+      self.log("Gather integration is ready.");
+      self.log("Game statistics", self.#game.getStats());
+    };
+  }
+
+  #gatherPlayerMoves() {
+    const self = this;
+    return (data, context) => {
+      if (self.me?.id == context.playerId) {
+        self.#playerPrivateSpaceId = null;
+      }
+    };
+  }
+
+  #gatherPlayerRings() {
+    const self = this;
+    return (data, context) => {
       self.log(`${context?.player?.name} is calling you.`);
-      incomingCallCard.trigger({ caller: context?.player?.name });
-    });
+      self.#incomingCallCard.trigger({ caller: context?.player?.name });
+    }
+  }
 
+  #gatherPlayerSetsIsAlone() {
+    const self = this;
 
-    game.subscribeToEvent("playerSetsIsAlone", (data, context) => {
-      if (avatarName != context?.player?.name) {
+    return (data, context) => {
+      const privateSpaceId = self.#gatherPlayerInPrivateSpace(context?.player);
+
+      if(!self.me) {
         return;
       }
 
-      isAlone = !!context?.player?.isAlone;
-
-      // You are not alone anymore
-      if (!isAlone) {
-        self.log("You are not alone anymore.");
-
-        // Raise an alert if you are away
-        if(context?.player?.away) {
-          notAloneCard.trigger();
-        }
+      if (self.me.id != context.playerId && privateSpaceId != self.myPrivateSpaceId) {
+        return;
       }
-    });
 
-    game.subscribeToEvent("playerWaves", (data, context) => {
-      self.log(`${context?.player?.name} waves to you.`);
-      incomingWaveCard.trigger({ person: context?.player?.name });
-    });
+      let others = "";
+      if (self.myPrivateSpaceId) {
+        others = self.#game.filterPlayersInSpace(player => player.id != self.me.id && self.#game.isPlayerInPrivateSpace(player, self.me.map, self.myPrivateSpaceId))
+          .map(player => player.name)
+          .join(', ');
+      }
 
-    this.log('Initialization of the Gather integration completed.');
+      if (self.me.isAlone) {
+        self.log(`You are now alone at '${self.myPrivateSpaceId}'.`);
+      } else {
+        self.log(`You are joined by ${others} at '${self.myPrivateSpaceId}'.`);
+        self.#presenceStatusCard.trigger({
+          alone: self.me.isAlone,
+          away: self.me.away,
+          persons: others
+        });
+      }
+    }
   }
 
+  #gatherPlayerWaves() {
+    const self = this;
+    return (data, context) => {
+      const targetId = context?.player?.targetId;
+
+      if (self.me?.id == targetId) {
+        self.log(`${context?.player?.name} waves to you.`);
+        self.#incomingWaveCard.trigger({ person: context?.player?.name });
+      } else {
+        const others = self.#game.filterPlayersInSpace(player => player.id == targetId);
+        const playerName = others.length == 1 ? others[0].name : "unknown";
+        self.log(`${context?.player?.name} waves to ${playerName}.`);
+      }
+    };
+  }
+
+  #gatherPlayerInPrivateSpace(player) {
+    const self = this;
+
+    if (!player) {
+      return null;
+    }
+
+    let map = null;
+
+    try {
+      map = self.#game.completeMaps[player.map];  
+    } catch (error) {
+      // This can occur in the beginning when the map isn't fully setup.
+    }
+
+    if (!map) {
+      return null;
+    }
+
+    for (const id in map.nooks) {
+      const nook = map.nooks[id];
+      if (!!nook.nookCoords.coords.some((c) => c.x === player.x && c.y === player.y)) {
+        return id;
+      }
+    }
+
+    return null;
+  }
 }
 
 module.exports = GatherApp;
